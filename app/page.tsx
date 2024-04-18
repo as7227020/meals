4065b7f39d346078cfaeabaa3b1863803c776845
Zin_User <as7227020@gmail.com>
2024-04-18 14:17:58 +0800

    增加分享功能 - react-share-kit

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,12 @@ import "./home.css";
 import Image from "next/image";
 import ScrollToTopBtn from "./ScrollToTopBtn";
 import toast from "react-hot-toast";
+import {
+  LineShare,
+  TwitterShare,
+  FacebookShare,
+  WhatsappShare,
+} from "react-share-kit";
 
 const FilterAssemblyData = () => {
   return {
@@ -251,6 +257,11 @@ export default function Home() {
     });
   };
 
+  const copylink = (link: string) => {
+    navigator.clipboard.writeText(link);
+    toast.success("リンクをコピーしました。");
+  };
+
   const [windowWidth, SetwindowWidth] = useState<number>(0);
   useEffect(() => {
     Get_CMS_Announcement_Setting();
@@ -485,7 +496,7 @@ export default function Home() {
                   </li>
                   <li className="list-group-item">
                     {" "}
-                    <div className="d-flex w-100 justify-content-between">
+                    <div className="d-flex w-100 justify-content-between align-items-center">
                       <small>
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
@@ -500,20 +511,100 @@ export default function Home() {
                         </svg>
                         　{data.tel}
                       </small>
-                      <a href={data.weblink} className="btn btn-info btn-sm">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="18"
-                          height="18"
-                          viewBox="5 10 512 512"
+                      <div className="btn-group dropstart">
+                        <button
+                          type="button"
+                          className="btn btn-info dropdown-toggle btn-sm"
+                          data-bs-toggle="dropdown"
+                          aria-expanded="false"
                         >
-                          <path
-                            fill="currentColor"
-                            d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0m0 64c35.4 0 64 28.6 64 64s-28.6 64-64 64s-64-28.6-64-64s28.6-64 64-64m85.3 384H170.7v-21.3H192V256h-21.3v-21.3H320v192h21.3z"
-                          />
-                        </svg>
-                        サイト
-                      </a>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            viewBox="0 0 24 24"
+                          >
+                            <g fill="none">
+                              <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                              <path
+                                fill="currentColor"
+                                d="M5.636 10.586a3.5 3.5 0 1 1 4.95-4.95l7.778 7.778a3.5 3.5 0 0 1-4.95 4.95l-4.066-4.066a1.25 1.25 0 1 1 1.768-1.768l3.712 3.713a1 1 0 0 0 1.415-1.415l-3.713-3.712a3.25 3.25 0 0 0-4.596 4.596L12 19.778A5.5 5.5 0 1 0 19.778 12L12 4.222A5.5 5.5 0 1 0 4.222 12l.353.353A1 1 0 1 0 5.99 10.94z"
+                              />
+                            </g>
+                          </svg>
+                        </button>
+                        <ul className="dropdown-menu">
+                          <li>
+                            <a
+                              className="dropdown-item d-flex justify-content-center align-items-center"
+                              href={data.weblink}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 512 512"
+                                style={{
+                                  color: "#2ec8f7",
+                                }}
+                              >
+                                <path
+                                  fill="currentColor"
+                                  d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0m0 64c35.4 0 64 28.6 64 64s-28.6 64-64 64s-64-28.6-64-64s28.6-64 64-64m85.3 384H170.7v-21.3H192V256h-21.3v-21.3H320v192h21.3z"
+                                />
+                              </svg>
+                              　Webサイト
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              className="dropdown-item gap-3 d-flex justify-content-between align-items-center"
+                              style={{ backgroundColor: "#ffe5fb" }}
+                            >
+                              <LineShare
+                                size={28}
+                                url={data.weblink}
+                                borderRadius={10}
+                                title={data.place + "　－　" + data.storename}
+                              />
+                              <TwitterShare
+                                size={28}
+                                url={data.weblink}
+                                borderRadius={10}
+                              />
+                              <FacebookShare
+                                size={28}
+                                url={data.weblink}
+                                borderRadius={10}
+                              />
+                              <WhatsappShare
+                                size={28}
+                                url={data.weblink}
+                                borderRadius={10}
+                              />
+                            </a>
+                          </li>
+                          <li>
+                            <a
+                              className="dropdown-item gap-3 d-flex justify-content-center align-items-center"
+                              onClick={(e) => copylink(data.weblink)}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                              >
+                                <path
+                                  fill="currentColor"
+                                  d="M5 5a4 4 0 0 0-4 4v6a4 4 0 0 0 4 4h4l-1-2H5a2 2 0 0 1-2-2V9a2 2 0 0 1 2-2h3l1-2zm11 2h3a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2h-3l-1 2h4a4 4 0 0 0 4-4V9a4 4 0 0 0-4-4h-4zm-8 6h8v-2H8z"
+                                />
+                              </svg>
+                              リンクをコピー
+                            </a>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </li>
                   <li className="list-group-item d-flex align-items-center">
